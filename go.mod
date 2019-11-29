cf371603dcf3b64cc089d09667a5fce6cc7d245c
Jorrit Salverda <jsalverda@travix.com>
2019-11-29 12:15:18 +0100

    cancel commands on sigterm

--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ go 1.13
 
 require (
 	github.com/alecthomas/kingpin v2.2.6+incompatible
-	github.com/estafette/estafette-foundation v0.0.32
-	github.com/rs/zerolog v1.16.0
+	github.com/estafette/estafette-foundation v0.0.37
+	github.com/rs/zerolog v1.17.2
 	github.com/stretchr/testify v1.2.2
 	gopkg.in/yaml.v2 v2.2.5
 )
--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"io/ioutil"
-	"log"
 	"runtime"
 
 	"github.com/alecthomas/kingpin"
 	foundation "github.com/estafette/estafette-foundation"
-	zerolog "github.com/rs/zerolog/log"
+	"github.com/rs/zerolog/log"
 	"gopkg.in/yaml.v2"
 )
 
@@ -37,78 +37,81 @@ func main() {
 	// init log format from envvar ESTAFETTE_LOG_FORMAT
 	foundation.InitLoggingFromEnv(appgroup, app, version, branch, revision, buildDate)
 
-	zerolog.Info().Msg("Unmarshalling parameters / custom properties...")
+	// create context to cancel commands on sigterm
+	ctx := foundation.InitCancellationContext(context.Background())
+
+	log.Info().Msg("Unmarshalling parameters / custom properties...")
 	var params params
 	err := yaml.Unmarshal([]byte(*paramsYAML), &params)
 	if err != nil {
-		log.Fatal("Failed unmarshalling parameters: ", err)
+		log.Fatal().Err(err).Msg("Failed unmarshalling parameters")
 	}
 
-	zerolog.Info().Msg("Setting defaults for parameters that are not set in the manifest...")
+	log.Info().Msg("Setting defaults for parameters that are not set in the manifest...")
 	params.SetDefaults(*releaseTargetName)
 
 	if *credentialsJSON == "" {
-		log.Fatal("Credentials of type kubernetes-engine are not injected; configure this extension as trusted and inject credentials of type kubernetes-engine")
+		log.Fatal().Err(err).Msg("Credentials of type kubernetes-engine are not injected; configure this extension as trusted and inject credentials of type kubernetes-engine")
 	}
 
-	log.Printf("Unmarshalling injected credentials...")
+	log.Info().Msg("Unmarshalling injected credentials...")
 	var credentials []GKECredentials
 	err = json.Unmarshal([]byte(*credentialsJSON), &credentials)
 	if err != nil {
-		log.Fatal("Failed unmarshalling injected credentials: ", err)
+		log.Fatal().Err(err).Msg("Failed unmarshalling injected credentials")
 	}
 
-	log.Printf("Checking if credential %v exists...", params.Credentials)
+	log.Info().Msgf("Checking if credential %v exists...", params.Credentials)
 	credential := GetCredentialsByName(credentials, params.Credentials)
 	if credential == nil {
-		log.Fatalf("Credential with name %v does not exist.", params.Credentials)
+		log.Fatal().Err(err).Msgf("Credential with name %v does not exist.", params.Credentials)
 	}
 
-	log.Printf("Retrieving service account email from credentials...")
+	log.Info().Msg("Retrieving service account email from credentials...")
 	var keyFileMap map[string]interface{}
 	err = json.Unmarshal([]byte(credential.AdditionalProperties.ServiceAccountKeyfile), &keyFileMap)
 	if err != nil {
-		log.Fatal("Failed unmarshalling service account keyfile: ", err)
+		log.Fatal().Err(err).Msg("Failed unmarshalling service account keyfile")
 	}
 	var saClientEmail string
 	if saClientEmailIntfc, ok := keyFileMap["client_email"]; !ok {
-		log.Fatal("Field client_email missing from service account keyfile")
+		log.Fatal().Msg("Field client_email missing from service account keyfile")
 	} else {
 		if t, aok := saClientEmailIntfc.(string); !aok {
-			log.Fatal("Field client_email not of type string")
+			log.Fatal().Msg("Field client_email not of type string")
 		} else {
 			saClientEmail = t
 		}
 	}
 
-	log.Printf("Storing gke credential %v on disk...", params.Credentials)
+	log.Info().Msgf("Storing gke credential %v on disk...", params.Credentials)
 	err = ioutil.WriteFile("/key-file.json", []byte(credential.AdditionalProperties.ServiceAccountKeyfile), 0600)
 	if err != nil {
-		log.Fatal("Failed writing service account keyfile: ", err)
+		log.Fatal().Err(err).Msg("Failed writing service account keyfile")
 	}
 
-	log.Printf("Authenticating to google cloud")
-	foundation.RunCommandWithArgs("gcloud", []string{"auth", "activate-service-account", saClientEmail, "--key-file", "/key-file.json"})
+	log.Info().Msg("Authenticating to google cloud")
+	foundation.RunCommandWithArgs(ctx, "gcloud", []string{"auth", "activate-service-account", saClientEmail, "--key-file", "/key-file.json"})
 
-	log.Printf("Setting gcloud account to %v", saClientEmail)
-	foundation.RunCommandWithArgs("gcloud", []string{"config", "set", "account", saClientEmail})
+	log.Info().Msgf("Setting gcloud account to %v", saClientEmail)
+	foundation.RunCommandWithArgs(ctx, "gcloud", []string{"config", "set", "account", saClientEmail})
 
-	log.Printf("Setting gcloud project")
-	foundation.RunCommandWithArgs("gcloud", []string{"config", "set", "project", credential.AdditionalProperties.Project})
+	log.Info().Msg("Setting gcloud project")
+	foundation.RunCommandWithArgs(ctx, "gcloud", []string{"config", "set", "project", credential.AdditionalProperties.Project})
 
-	log.Printf("Getting gke credentials for cluster %v", credential.AdditionalProperties.Cluster)
+	log.Info().Msgf("Getting gke credentials for cluster %v", credential.AdditionalProperties.Cluster)
 	clustersGetCredentialsArsgs := []string{"container", "clusters", "get-credentials", credential.AdditionalProperties.Cluster}
 	if credential.AdditionalProperties.Zone != "" {
 		clustersGetCredentialsArsgs = append(clustersGetCredentialsArsgs, "--zone", credential.AdditionalProperties.Zone)
 	} else if credential.AdditionalProperties.Region != "" {
 		clustersGetCredentialsArsgs = append(clustersGetCredentialsArsgs, "--region", credential.AdditionalProperties.Region)
 	} else {
-		log.Fatal("Credentials have no zone or region; at least one of them has to be defined")
+		log.Fatal().Err(err).Msg("Credentials have no zone or region; at least one of them has to be defined")
 	}
-	foundation.RunCommandWithArgs("gcloud", clustersGetCredentialsArsgs)
+	foundation.RunCommandWithArgs(ctx, "gcloud", clustersGetCredentialsArsgs)
 
-	zerolog.Info().Msgf("Forwarding port %v to port %v on service %v in namespace %v...", params.LocalPort, params.ServicePort, params.Service, params.Namespace)
-	foundation.RunCommand("kubectl port-forward service/%v %v:%v --address=0.0.0.0 -n %v", params.Service, params.LocalPort, params.ServicePort, params.Namespace)
+	log.Info().Msgf("Forwarding port %v to port %v on service %v in namespace %v...", params.LocalPort, params.ServicePort, params.Service, params.Namespace)
+	foundation.RunCommand(ctx, "kubectl port-forward service/%v %v:%v --address=0.0.0.0 -n %v", params.Service, params.LocalPort, params.ServicePort, params.Namespace)
 
 	// wait for SIGTERM
 	sigs, wg := foundation.InitGracefulShutdownHandling()
